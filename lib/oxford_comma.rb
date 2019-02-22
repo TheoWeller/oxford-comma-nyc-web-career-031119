7c2e1fe4300384fb8f79ba11de1d162ae5dcd44f
Theo Weller <weller.theodor@gmail.com>
2019-02-22 23:26:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-  if array.size <= 1
-  array.join
-elsif array.size == 2
-  array.join(" and ")
-else
-  if 2 < array.length
+
+  return array.join(' and ') if array.size == 2
+
+    if 2 < array.length
     array[-1].insert(0, "and ")
   end
-end
+   array.join(", ")
 end
